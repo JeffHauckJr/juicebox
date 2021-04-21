e9f1f81ce19bcf7f1613322c0152267014ae0ab1
JeffHauckJr <jefferyhauckjr@gmail.com>
2021-04-20 21:37:36 -0500

    Final Commit Part 1 Juicebox 2

--- api/index.js
+++ api/index.js
@@ -1,17 +1,3 @@
-// const express = require('express');
-// const apiRouter = express.Router();
-
-// const usersRouter = require('./users');
-// apiRouter.use('/users', usersRouter);
-
-// const postsRouter = require('./posts');
-// apiRouter.use('/posts', postsRouter);
-
-// // const tagsRouter = require('./tags');
-// // apiRouter.use('/tags', tagsRouter);
-
-// module.exports = apiRouter;
-
 const express = require('express');
 const apiRouter = express.Router();
 
